68c27185f94de6d86cf24790d8c7a55d937f1376
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-05-02 23:49:53 +0530

    Operations on Variables

---
+++ Operations on Variables/sequence_for_computation.py
@@ -0,0 +1,25 @@
+#PEDMAS
+
+#Example1:Parentheses
+result_1=(2+3)*4
+print(result_1) #Output:20
+
+
+#Example2:Exponentiation 
+result_2=2**3*4
+print(result_2)  #Output:32
+
+
+#Example3:Multiplication and Division
+result_3=10/2*3
+print(result_3)  #Output:15.0
+
+
+#Example4:Addition and Subtraction
+result_4=10-5+2
+print(result_4)  #Output:7
+
+
+#Example5:Mixed Operations
+result_5=2*(3+4)**2
+print(result_5)  #Output:98
---
+++ Operations on Variables/string_operations.py
@@ -0,0 +1,24 @@
+str1="Hello"
+str2="World"
+result=str1+str2
+print(result)     #Output:HelloWorld
+
+
+str1="Hello"
+str2=" World"
+result=str1+str2
+print(result)   #Output:Hello World
+
+
+
+word="Python"
+multiplied=word*3
+print(multiplied) #Output:PythonPythonPython
+
+
+str1="Hello"
+str2="World"
+result=str1*str2
+print(result)   #This will raise a TypeError
+
+
