635412acb20eb3423b1eabe53e32b77439058fab
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-06-25 23:50:19 +0530

    Comments

---
+++ 01_Comments/_01_single_line_comments.py
@@ -0,0 +1,5 @@
+# Single-Line Comments
+
+# This is a single_line comment
+x = 10  # Assigning the value 10 to variable x
+
---
+++ 01_Comments/_02_mutli-line_comments.py
@@ -0,0 +1,11 @@
+# Mutli-Line Comments
+
+"""
+
+This is a multi-line comment(block comment).
+It spans multiple lines and is enclosed in triple quotes
+While technically a string ,it serves the purpose of a comment.
+
+"""
+
+x = 20  # Assigning the value 20 to variable x
