289944ad0842dfcc5ba474627ca8911232b7368f
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-06-26 01:10:22 +0530

    Variables

--- 02_Variables/_12_area_of_rectangle_by_taking_input.py
+++ 02_Variables/_12_area_of_rectangle_by_taking_input.py
@@ -1,13 +1,13 @@
-#Script to Calculate the Area of a Rectangle
+# Script to Calculate the Area of a Rectangle
 
-#Take input from the user of length and width of the rectangle
+# Take input from the user of length and width of the rectangle
 
-length=float(input("Enter the length of the rectangle:"))
-breadth=float(input("Enter the breadth of the rectangle:"))
+length = float(input("Enter the length of the rectangle:"))
+breadth = float(input("Enter the breadth of the rectangle:"))
 
 
-#Calculate the area
-area=length*breadth
+# Calculate the area
+area = length * breadth
 
-#Print the result
-print("The area of the rectangle is:",area) 
+# Print the result
+print("The area of the rectangle is:", area)
