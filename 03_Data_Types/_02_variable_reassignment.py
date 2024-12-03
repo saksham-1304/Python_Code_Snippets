bafa54285c3cdb218a63acf1cc38f1691b056655
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-12-04 03:39:48 +0530

    Data Types

---
+++ 03_Data_Types/_02_variable_reassignment.py
@@ -0,0 +1,8 @@
+# Variable Reassignment 
+
+x=0
+print(x) # Output: 0
+
+# Assigning string variable 
+x="hello"
+print(x) # Output: hello
---
+++ 03_Data_Types/_05_int_data_type.py
@@ -0,0 +1,15 @@
+# assign an int object to a variable
+x=5
+#assign another variable with this
+y=x
+#update the previous int object 
+x=10
+#print the value contained in each
+
+print(x) # output 10
+print(y) # output 5
+
+a=5 #binary:0101
+b=4 #binary:0100
+result=a&b
+print(result) #output:4
